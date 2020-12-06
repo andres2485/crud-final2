e2b3a0ea6553767a276eceec1cbea6cc5479f565
andres2485 <claudio.quezada@virginiogomez.cl>
2020-12-06 20:31:21 -0300

    asdasdasd

---
+++ demo_crud/demo_crud/Models/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace demo_crud.Models
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(25, MinimumLength =3)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Apellido{ get; set; }
+
+
+        [Required]
+        [Range(0,99)]
+        public int Edad { get; set; }
+        [Required]
+        [StringLength(25, MinimumLength = 10)]
+        public string Direccion { get; set; }
+
+
+        [Required]
+        [Range(100000000, 999999999, ErrorMessage = "Teléfono debe contener 9 dígitos")]
+        public int Telefono { get; set; }
+
+    }
+}
